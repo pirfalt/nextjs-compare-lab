75dcef6e3e3c08559d7292a3009fdfe00f95e1d9
Emil Pirfält <emil.pirfalt@gmail.com>
2022-03-23 16:40:31 +0100

    Migrate Card styles to style jsx element

--- next-example-typescript/components/Card.tsx
+++ next-example-typescript/components/Card.tsx
@@ -1,4 +1,3 @@
-import styles from "../styles/Home.module.css";
 import logo from "./logo512.png";
 import Image from "next/image";
 
@@ -6,12 +5,46 @@ export default function Card(props: {
   title: string;
   description: string;
   href: string;
+  skipImg?: boolean;
 }) {
   return (
-    <a className={styles.card} href={props.href}>
+    <a className="card" href={props.href}>
+      <style jsx>
+        {`
+          .card {
+            margin: 1rem;
+            padding: 1.5rem;
+            text-align: left;
+            color: inherit;
+            text-decoration: none;
+            border: 1px solid #eaeaea;
+            border-radius: 10px;
+            transition: color 0.15s ease, border-color 0.15s ease;
+            max-width: 300px;
+          }
+
+          .card:hover,
+          .card:focus,
+          .card:active {
+            color: #0070f3;
+            border-color: #0070f3;
+          }
+
+          .card h2 {
+            margin: 0 0 1rem 0;
+            font-size: 1.5rem;
+          }
+
+          .card p {
+            margin: 0;
+            font-size: 1.25rem;
+            line-height: 1.5;
+          }
+        `}
+      </style>
       <h2>{props.title} &rarr;</h2>
       <p>{props.description}</p>
-      <Image alt="React logo" {...logo} />
+      {props.skipImg ? null : <Image alt="React logo" {...logo} />}
     </a>
   );
 }
--- next-example-typescript/pages/index.tsx
+++ next-example-typescript/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Card from "../components/Card";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -24,39 +25,40 @@ const Home: NextPage = () => {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Card
+            title="Documentation &rarr;"
+            href="https://nextjs.org/docs"
+            description="Find in-depth information about Next.js features and API."
+            skipImg={true}
+          />
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <Card
+            title="Learn &rarr;"
+            href="https://nextjs.org/learn"
+            description="Learn about Next.js in an interactive course with quizzes!"
+            skipImg={true}
+          />
 
-          <a
+          <Card
+            title="Examples &rarr;"
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            description="Discover and deploy boilerplate example Next.js projects."
+            skipImg={true}
+          />
 
-          <a
+          <Card
+            title="Deploy &rarr;"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            description="Instantly deploy your Next.js site to a public URL with Vercel."
+            skipImg={true}
+          />
 
-          <Link href="/about">
-            <a className={styles.card}>
-              <h2>About &rarr;</h2>
-              <p>Magic route! 🤯</p>
-            </a>
+          <Link href="/about" passHref={true}>
+            <Card
+              title="About &rarr;"
+              href="/about"
+              description="Magic route! 🤯"
+            />
           </Link>
         </div>
       </main>
